cb0a5c1a690dcd90366b56469f62656ad845b1b4
Bhagya <bhagyamadhawa@yahoo.com>
2021-02-28 11:13:55 +0530

    Add Cart Buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { commerce } from "./lib/commerce";
 
 import {  Products, Navbar, Cart } from "./Components";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Product from './Components/Products/Product/Product';
 
 const App = () => {
 
@@ -21,8 +22,28 @@ const App = () => {
     }
 
     const handleAddToCart = async(productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+        setCart(cart);
+    }
+
+    const handleUpdateCartQty = async (productId, quantity ) => {
+        // const response = await commerce.cart.update(productId, {quantity});
+        // setCart(response.cart);
+
+        const { cart } = await commerce.cart.update(productId, {quantity});
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+        setCart(cart);
+    }
+
+    const handleEmptyCart = async () => {
+        
+        const { cart } = await commerce.cart.empty();
+        setCart(cart);
+
     }
 
     useEffect(() => {
@@ -40,7 +61,11 @@ const App = () => {
                     </Route>
 
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
                     </Route>
                     
                 </Switch>
